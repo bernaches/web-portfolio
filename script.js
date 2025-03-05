d1e8ce8d752d4642a19ee6a978f39cbea8a05264
bernaches <157933720+bernaches@users.noreply.github.com>
2025-03-05 16:03:26 +0800

    Update script.js
    got the carousel working finally

--- script.js
+++ script.js
@@ -10,94 +10,41 @@ const changeFavicon = () => {
 };
 changeFavicon();
 
-// const images = document.querySelectorAll(".screenshot-img .img");
-// let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  // Initialize all carousels
+  const carousels = document.querySelectorAll(".project-screenshot");
 
-// var next = document.querySelector(".button.next");
-// var prev = document.querySelector(".button.back");
-// var imgSrc = document.querySelectorAll(".img");
-// var pageNum = document.querySelector(".page");
-// next.addEventListener("click", showNextImage);
-// prev.addEventListener("click", showPreviousImage);
+  carousels.forEach((carousel) => {
+    let imageIndex = 0;
+    const images = carousel.querySelectorAll(".screenshot-img .img");
+    const pageCounter = carousel.querySelector(".page");
+    const totalImages = images.length;
 
-// pageNum.textContent = `${currentIndex + 1} / ${imgSrc.length - 1}`;
-// console.log(`index outside: ${currentIndex}`);
-// function showNextImage() {
-//   // Remove the "show" class from the current image
-//   images[currentIndex].classList.remove("show");
-//   console.log(`current index: ${currentIndex}`);
+    function updateCarousel(n) {
+      if (n >= totalImages) {
+        imageIndex = 0;
+      } else if (n < 0) {
+        imageIndex = totalImages - 1;
+      } else {
+        imageIndex = n;
+      }
 
-//   // Increment the index to move to the next image
-//   currentIndex = (currentIndex + 1) % images.length;
-//   console.log(`next index: ${currentIndex}`);
+      // Hide all images and show only the current one
+      images.forEach((img) => img.classList.remove("show"));
+      images[imageIndex].classList.add("show");
 
-//   // Add the "show" class to the next image
-//   images[currentIndex].classList.add("show");
-//   console.log(`next img called ${currentIndex}`);
+      // Update the page counter
+      pageCounter.textContent = `${imageIndex + 1} / ${totalImages}`;
+    }
 
-//   // Update page count in DOM
-//   pageNum.textContent = `${currentIndex + 1} / ${imgSrc.length - 1}`;
-//   console.log(`final index: ${currentIndex + 1}`);
-// }
-// function showPreviousImage() {
-//   images[currentIndex].classList.remove("show");
-//   currentIndex = (currentIndex - 1 + images.length) % images.length;
-//   // pageNum.textContent = `${currentIndex + 1} / ${imgSrc.length - 1}`;
-//   images[currentIndex].classList.add("show");
-//   console.log(`back img called`);
-// }
+    // Initialize this carousel
+    updateCarousel(imageIndex);
 
-// Select all elements with the class 'project-content'
-// const projectContents = document.querySelectorAll(".project-content");
+    // Add event listeners for buttons
+    const backButton = carousel.querySelector(".button.back");
+    const nextButton = carousel.querySelector(".button.next");
 
-// // Loop through each 'project-content' element
-// projectContents.forEach((projectContent, index) => {
-//   // Add a click event listener to each 'project-content'
-//   projectContent.addEventListener("click", () => {
-//     // Log which 'project-content' was clicked
-//     console.log(`Clicked on project-content ${index + 1}`);
-
-//     // You can also access specific elements within the clicked 'project-content'
-//     const headingTitle =
-//       projectContent.querySelector(".heading-title").textContent;
-//     console.log(`Project Title: ${headingTitle}`);
-
-//     const prev = projectContent.querySelectorAll(".button.back");
-//     const next = projectContent.querySelectorAll(".button.next");
-//     prev.forEach((p) => {
-//       p.addEventListener("click", () => {
-//         console.log(`${p}`);
-//       });
-//     });
-//     next.forEach((n) => {
-//       n.addEventListener("click", () => {
-//         console.log(`You clicked next`);
-//       });
-//     });
-
-//     // console.log(`button: ${buttons}`);
-//   });
-// });
-
-let imageIndex = 0;
-showImage(imageIndex);
-
-function plusImage(n) {
-  showImage((imageIndex += n));
-}
-function currentImage(n) {
-  showIage((imageIndex = n));
-}
-
-function showImage(n) {
-  console.log(`current: ${imageIndex}`);
-  var i;
-  var images = document.querySelectorAll(".screenshot-img .img");
-
-  if (n >= images.length) {
-    imageIndex = 0;
-    console.log(`new: ${imageIndex}`);
-  }
-
-  images[imageIndex].classList.add("show");
-}
+    backButton.addEventListener("click", () => updateCarousel(imageIndex - 1));
+    nextButton.addEventListener("click", () => updateCarousel(imageIndex + 1));
+  });
+});
